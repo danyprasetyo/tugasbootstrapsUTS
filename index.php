2c4717d1879d97d30ebad3c72398ad55d30fc346
dany <danyprsfw@gmail.com>
2022-11-03 16:22:40 +0700

    tugasUTS

---
+++ index.php
@@ -0,0 +1,230 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tugas Ujian Tengah Semester</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  </head>
+
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+  <body id="page-top ">
+    <nav id="navbar-custom" class="navbar navbar-dark navbar-expand-lg fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#page-top">DPF-GARAGE</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#carouselExampleCaptions">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Information">Info</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#location">Location</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#testi">Social Media</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      </div>
+    </nav>
+
+    <!-- HOME -->
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="images/img-1.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>DPF-GARAGE</h5>
+            <p>Sebuah Bisnis Kecil yaitu Detailing Mobil yang Dimiliki oleh Dany Prasetyo Fawzi</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="images/img-2.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>DPF-GARAGE</h5>
+            <p>Dikerjakan menggunakan alat proper.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="images/img-3.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>DPF-GARAGE</h5>
+            <p>Kualitas Bintang Lima Harga Kaki Lima.</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
+    <!-- About -->
+      <section class="min-vh-100 d-flex align-items-center  bg-dark" id="about">
+        <div class="container"><br>
+            <h1 class="text-center" style="padding-bottom:30px;">About</h1>
+            <br>
+
+          <div class="row row-cols-2">
+            <div class="col" data-aos="fade-right" data-aos-duration="2000">
+              <h1 class="main-header text-white">
+                DPF-GARAGE
+                <br>
+                <span class="text-warning">Dany Prasetyo Fawzi</span>
+              </h1>
+              <p class="my-4 text-white ">
+                Halo! Saya Dany, saya adalah seorang mahasiswa yang kebetulan memiliki Bisnis Kecil Detailing Mobil. DPF-Garage adalah singkatan nama panjang saya, Berawal dari Storan bulanan sudah mepet saya bingung cari cuan kemana lagi saya berinisiatif untuk membuka usaha kecil yang berlokasi dirumah sendiri, dan alhamdulillah meski baru memiliki 3 Pelanggan kami sangat bangga memiliki usaha ini.
+              </p>
+              <a class="btn btn-warning btn-lg shadow-lg" href="#kontak">Hubungi Saya</a>
+            </div>
+            <div class="col" data-aos="fade-left" data-aos-duration="2000">
+              <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img src="images/dany.jpg" class="img-responsive" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="images/dany2.jpg" class="d-block w-100 img-responsive" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="images/img-3.jpg" class="d-block w-100 img-responsive" alt="...">
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div> 
+        </div>
+      </section>
+
+      <!-- informasi -->
+    <section id="Information">
+        <div class="container"><br><br><br>
+          <h1 class="text-center" style="padding-bottom:30px;">Information</h1>
+          <div class="row gy-4" >
+            <div class=" col-lg-4 col col-sm-6 col-12" data-aos="fade-right" data-aos-duration="2000">
+              <div class="card h-100">
+                <div class="ratio ratio-16x9">
+                  <img src="images/mezerna.jpg" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title fw-bold">Detailing Exterior</h5>
+                  <p class="card-text text-content">
+                    Dengan menggunakan 3 Steps Polish (High Cuting, Medium Cuting, Finishing), kami menggunakan produk terbaik tahun ini yaitu Menzerna.
+                  </p>
+                </div>                
+               </div>
+            </div>
+            <div class=" col-lg-4 col col-sm-6 col-12" >
+              <div class="card h-100">
+                <div class="ratio ratio-16x9">
+                  <img src="images/ngs.jpg" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title fw-bold">New Graphene Spray</h5>
+                  <p class="card-text text-content">
+                    adalah produk Semi-coating andalan kami untuk Finishing Exterior, digunakan untuk melindungi permuka Cat, memiliki Efek Daun Talas, sehingga terlihat seperti sudah dicoating.
+                  </p>
+                </div>
+                
+              </div>
+            </div>
+            <div class=" col-lg-4 col col-sm-6 col-12" data-aos="fade-left" data-aos-duration="2000">
+              <div class="card h-100">
+                <div class="ratio ratio-16x9">
+                  <img src="images/exterior.jpg" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title fw-bold">Detailing Engine</h5>
+                  <p class="card-text text-content">
+                    Dengan Produk kami sendiri, kami menggunakan All-Purpose yang bisa digunakan kepada kotoran di ruang mesin dan Interior.
+                  </p>
+                </div>
+               </div>
+            </div>
+          </div> 
+        </div>
+    <section>
+
+        <!-- Testi -->
+    <section class="my-5" id="location">
+        <div class="container">
+          <h1 class="text-center" style="padding-bottom:30px;">Location</h1>
+          <p class="text-center mb-4 text-content">
+            - Perumahan Kotabaru Cilaku, Cianjur -
+          </p>
+          <div>
+            <iframe cen src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d990.2901009181994!2d107.12812942919713!3d-6.87137536821757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68516fc630975b%3A0x32f3c9afdf7b2c77!2sDPF%20GARAGE!5e0!3m2!1sid!2sid!4v1667391548832!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+          </div>
+        </div>
+      </section>
+    </section>
+
+    <!-- testi -->
+        <section id="testi">
+            <h1 class="text-center" style="padding-bottom:30px;">Social Media TikTok</h1>             
+                    <div class="embed-responsive embed-responsive-16by9">
+                       <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@ystrdyafternoonboys/video/7137590250624339226" data-video-id="7137590250624339226" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@ystrdyafternoonboys" href="https://www.tiktok.com/@ystrdyafternoonboys?refer=embed">@ystrdyafternoonboys</a> <p>Treatment exterior Kijang Super 3 door. ✨</p> <a target="_blank" title="♬ suara asli  - Cleary" href="https://www.tiktok.com/music/suara-asli-Cleary-7123460417078233882?refer=embed">♬ suara asli  - Cleary</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>
+                    
+            </div>
+        </section>
+<!-- contact -->
+    <section id="contact">
+        <div class="card bg-black  text-center text-white">
+            <div class="card-body">
+            Contact Me
+            <p class="text-warning">
+                Instagram : @dpfgarage | Email : dpf369@gmail.com
+            </p>
+          </div>
+          <div class="card-header">
+            Dany Prasetyo Fawzi
+          </div>
+        </div>
+    </section>
+
+
+
+    <!-- script -->
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+        let navbar = document.getElementById('navbar-custom');
+
+        window.addEventListener('scroll', function(){
+            let scroll = window.scrollY;
+
+            if (scroll > 50) {
+                navbar.classList.add('bg-black');
+            } else {
+                navbar.classList.remove('bg-black');
+            }
+        })
+    </script>
+</body>
+</html>
